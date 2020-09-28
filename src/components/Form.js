438cbec6b15fd3a8a36fa1f77272d9d81e7c29ab
Shaunak <shaunaksompura@gmail.com>
2020-09-28 08:55:37 -0400

    Edit feature done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 
 function Form(props) {
-
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
 
   function handleChange(e) {
-  setName(e.target.value);
+    setName(e.target.value);
   }
+
   function handleSubmit(e) {
-  e.preventDefault();
-  props.addTask(name);
-  setName("");
-}
+    e.preventDefault();
+    props.addTask(name);
+    setName("");
+  }
   return (
     <form onSubmit={handleSubmit}>
       <h2 className="label-wrapper">
