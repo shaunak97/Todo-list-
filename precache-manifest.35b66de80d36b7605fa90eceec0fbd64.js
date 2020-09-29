a77b135bc5698d13256056cf255ac7f1730b65c5
Shaunak <shaunaksompura@gmail.com>
2020-09-29 09:33:26 -0400

    Updates

---
+++ precache-manifest.35b66de80d36b7605fa90eceec0fbd64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b96da3f2af9f4b4d95b127d19b23d7",
+    "url": "/Todo-list-/index.html"
+  },
+  {
+    "revision": "e67cc1fa83d4a6ada131",
+    "url": "/Todo-list-/static/css/main.220fcbac.chunk.css"
+  },
+  {
+    "revision": "9e3cdfe2c7343a015e9a",
+    "url": "/Todo-list-/static/js/2.0c51fe6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todo-list-/static/js/2.0c51fe6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e67cc1fa83d4a6ada131",
+    "url": "/Todo-list-/static/js/main.955a1210.chunk.js"
+  },
+  {
+    "revision": "efaf459eef3d99021144",
+    "url": "/Todo-list-/static/js/runtime-main.93ba48a3.js"
+  }
+]);
